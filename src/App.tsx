fcc5dd5ef658cebf4ea6f52e50bf0392a65d05cd
Joe Fallon <joseph.t.fallon@gmail.com>
2018-10-29 14:03:45 -0700

    Code for React.lazy and Suspense was added. Types are still missing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 require('./App.css');
 
 import * as React from 'react';
+import { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router';
 
 import { IndexRouteProps } from './routes/index/IndexRouteProps';
 import { Route1Props } from './routes/route-1/Route1Props';
 import { Route2Props } from './routes/route-2/Route2Props';
 import { Route3Props } from './routes/route-3/Route3Props';
-
-import Loadable from 'react-loadable';
 
 
 export class App extends React.Component {
@@ -20,7 +19,7 @@ export class App extends React.Component {
     public render(): JSX.Element {
         return (
             <Switch>
-                <Route exact path='/'           render={(props) => this.renderIndex(props)}/>
+                <Route exact path='/'        render={(props) => this.renderIndex(props)}/>
                 <Route exact path='/route-1' render={(props) => this.renderRoute1(props)}/>
                 <Route exact path='/route-2' render={(props) => this.renderRoute2(props)}/>
                 <Route exact path='/route-3' render={(props) => this.renderRoute3(props)}/>
@@ -30,47 +29,51 @@ export class App extends React.Component {
     }
 
     private renderIndex = (props: IndexRouteProps): JSX.Element => {
-        const LoadableIndexRoute = Loadable({
-            loader: () => import(/* webpackChunkName: "index-route" */'./routes/index/IndexRoute'),
-            loading: () => { return <div>Loading...</div>; },
-            delay: 300,
-            timeout: 10000
+        const LoadableIndexRoute = lazy(() => {
+            return import(/* webpackChunkName: "index-route" */'./routes/index/IndexRoute');
         });
 
-        return (<LoadableIndexRoute {...props} />);
+        return (
+            <Suspense fallback={<div>Loading...</div>}>
+                <LoadableIndexRoute {...props} />
+            </Suspense>
+        );
     };
 
     private renderRoute1 = (props: Route1Props): JSX.Element => {
-        const LoadableRoute1 = Loadable({
-            loader: () => import(/* webpackChunkName: "route-1" */'./routes/route-1/Route1'),
-            loading: () => { return <div>Loading...</div>; },
-            delay: 300,
-            timeout: 10000
+        const LazyRoute = lazy(() => {
+            return import(/* webpackChunkName: "route-1" */'./routes/route-1/Route1');
         });
 
-        return (<LoadableRoute1 {...props} />);
+        return (
+            <Suspense fallback={<div>Loading...</div>}>
+                <LazyRoute {...props} />
+            </Suspense>
+        );
     };
 
     private renderRoute2 = (props: Route2Props): JSX.Element => {
-        const LoadableRoute2 = Loadable({
-            loader: () => import(/* webpackChunkName: "route-2" */'./routes/route-2/Route2'),
-            loading: () => { return <div>Loading...</div>; },
-            delay: 300,
-            timeout: 10000
+        const LazyRoute = lazy(() => {
+            return import(/* webpackChunkName: "route-2" */'./routes/route-2/Route2');
         });
 
-        return (<LoadableRoute2 {...props} />);
+        return (
+            <Suspense fallback={<div>Loading...</div>}>
+                <LazyRoute {...props} />
+            </Suspense>
+        );
     };
 
     private renderRoute3 = (props: Route3Props): JSX.Element => {
-        const LoadableRoute3 = Loadable({
-            loader: () => import(/* webpackChunkName: "route-3" */'./routes/route-3/Route3'),
-            loading: () => { return <div>Loading...</div>; },
-            delay: 300,
-            timeout: 10000
+        const LazyRoute = lazy(() => {
+            return import(/* webpackChunkName: "route-3" */'./routes/route-3/Route3');
         });
 
-        return (<LoadableRoute3 {...props} />);
+        return (
+            <Suspense fallback={<div>Loading...</div>}>
+                <LazyRoute {...props} />
+            </Suspense>
+        );
     };
 
     private renderNotFound = (props: any): JSX.Element => {
